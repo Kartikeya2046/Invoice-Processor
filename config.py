bb5be0f4d4a897a7cb91eeec7cb85dc945119a69
kartikeyaguptar-maker <katikeyagupta.r@gmail.com>
2026-05-29 12:59:49 +0530

    Fix Mongo URI fallback

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://guptakartikeya2046_db_user:<Kartikeya.2046>@cluster0.ivw6vlc.mongodb.net/")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "invoice_processor")
 TESSERACT_PATH = os.getenv("TESSERACT_PATH", "tesseract")
 
